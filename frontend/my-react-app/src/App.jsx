87095050577d1594ebc6c373a234b5b4fc42a17a
Matheus TI- Developer <178905543+MatheusViniciusDiasAlves@users.noreply.github.com>
2026-08-14 16:58:28 -0300

    Cria componente App com titulo

---
+++ frontend/my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+function Projeto({ ____, ____ }) {        // as duas props
+  return (
+    <div>
+      <h2>{ ____ }</h2>
+      <p>{ ____ }</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <___>                                  {/* UM elemento raiz */}
+      <h1>Matheus Vinicius Dias Alves</h1>
+
+      <Projeto titulo="____" tecnologias="____" />
+      {/* mais duas vezes, com valores diferentes */}
+    </___>
+  );
+}
+
+export default App;
